2fb21853a96393ae71c332bf76c21b6d65615104
Jason Xu <lemonade3532@outlook.com>
2019-01-26 16:57:55 -0800

    Update Sprite and added some code

---
+++ Assets/Scripts/LockedDoor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+	private Prop prop;
+	public bool unlock = false;
+	private bool isDone = false;
+
+	public Sprite unlockedState;
+
+	private void Start() {
+		prop = GetComponent<Prop>();
+
+		prop.isLocked = true;
+	}
+
+	private void Update() {
+		// unlock = LevelManager.childFound == 2
+		if (unlock && !isDone) {
+			prop.isLocked = false;
+			prop.UpdateSprite(unlockedState);
+
+			isDone = true;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Prop.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+	[HideInInspector] public bool hasChanged;
+	public bool isLocked = false;
+	public Sprite altState;
+
+	public string[] textDisplay;
+
+	private SpriteRenderer spriteRenderer;
+
+	private void Awake() {
+		hasChanged = false;
+		if (altState == null) {
+			Debug.LogError(gameObject.name + " has no alternated state sprite");
+		}
+
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void UpdateSprite(Sprite s) {
+		spriteRenderer.sprite = s;
+	}
+	private void OnMouseDown() {
+		Alternate();
+	}
+
+	public void Alternate() {
+		Debug.Log(gameObject.name + ", " + hasChanged + isLocked);
+		if (!hasChanged && !isLocked) {
+			hasChanged = true;
+
+			print("sprite change");
+			spriteRenderer.sprite = altState;
+		}
+	}
+}
